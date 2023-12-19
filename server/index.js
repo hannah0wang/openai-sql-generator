f3462c623716aa9125eda1debd9411535244f19b
Hannah Wang <hannah.a.wang@vanderbilt.edu>
2023-12-19 17:46:36 -0600

    feat: add index.js

---
+++ server/index.js
@@ -0,0 +1,30 @@
+import express from "express";
+import cors from 'cors';
+
+const app = express();
+app.use(express.json());
+
+app.use(cors());
+
+const port = process.env.PORT || 3006;
+
+import generate from "./generate.js";
+
+app.get("/", (req, res) => {
+    res.send("Hello world from our API");
+})
+
+app.post("/generate", async (req, res) => {
+    const queryDescription = req.body.queryDescription;
+    try {
+        const sqlQuery = await generate(queryDescription);
+        res.json({ response: sqlQuery });
+    } catch (error) {
+        console.error(error);
+        res.status(500).send("Internal Server Error");
+    }
+})
+
+app.listen(port, () => {
+    console.log(`Listening on port ${port}`);
+})
