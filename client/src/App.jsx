a3860d52c53e276e7d04d6124267529c47c0408c
Hannah Wang <hannah.a.wang@vanderbilt.edu>
2023-12-19 17:33:53 -0600

    feat(client): create react component

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,35 +1,49 @@
+import styles from './index.module.css'
+import sqlLogo from './assets/sql-logo.png'
+
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [queryDescription, setQueryDescription] = useState("");
+  const [sqlQuery, setSqlQuery] = useState("");
+
+  const onSubmit = async (e) => {
+    e.preventDefault();
+    const generatedQuery = await generateQuery();
+    setSqlQuery(generatedQuery);
+    console.log("returned from server:", generatedQuery);
+  };
+
+  const generateQuery = async () => {
+    const response = await fetch("http://localhost:3005/generate", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json"
+      },
+      body: JSON.stringify({ queryDescription: queryDescription }),
+    });
+
+    const data = await response.json();
+    return data.response.trim();
+  }
 
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+   <main className={styles.main}>
+    <img src={sqlLogo} alt="" className={styles.icon} />
+    <h3>Generate SQL with AI</h3>
+
+    <form onSubmit={onSubmit}>
+      <input
+      type="text"
+      name="query-description"
+      placeholder="Describe your query"
+      onChange={(e) => setQueryDescription(e.target.value)}
+      />
+      <input type="submit" value="Generate query"/>
+      <pre>{sqlQuery}</pre>
+    </form>
+   </main>
   )
 }
 
-export default App
+export default App;
